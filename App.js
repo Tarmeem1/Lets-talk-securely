30435b609692bbb95e2c30e0213f06e5e549829a
Tarmeem1 <mirtarmeem358@gmail.com>
2025-05-16 21:59:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Let's Talk Securely</h1>
+      <p>This is your secure private chat interface.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Let's Talk Securely
+
+A fixed version with working React setup.
